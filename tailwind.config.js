11bc956c88b2d5d9b17a3ba431b294100a9e9afc
Raajesh3108 <prajapatirajesh2003@gmail.com>
2024-06-29 11:02:28 +0530

    Finalized tailwind project setup with build and watch scripts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: [
-      './src/**/*.{html,js}',
-      './node_modules/flowbite/**/*.js'
+      './**/*.{html,js}',
     ],
     theme: {
+      screens: {
+        tl: { max: "1100px" },
+        ms: { max: "950px" },
+        cs: { max: "780px" },
+        sm: { max: "639px" }
+      },
       extend: {},
     },
-    plugins: [
-      require('daisyui'),
-      require('flowbite/plugin')
-    ],
-  }
-  
+    plugins: [require("daisyui")],
+  };
